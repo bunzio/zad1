265fd68974a23b84b2741087d1aedcf50654d5d3
Bunzio <bunzeladam@gmail.com>
2020-03-08 17:08:25 +0100

    zliczanie ilości wciśnięć przycisku

--- main.c
+++ main.c
@@ -1,18 +1,36 @@
 #include <msp430.h>
 
-int main(void)
-{
-  WDTCTL = WDTPW + WDTHOLD;                 // Stop watchdog timer
-  P1DIR |= 0x01;                            // Set P1.0 to output direction
+#define Przycisk      BIT3                        // Przyciskitch -> P1.3
+#define LedRed     BIT0                        // LedRed LED -> P1.0
+#define LedGreen      BIT6                        // LedGreen LED -> P1.6
 
-  for (;;)
-  {
-    volatile unsigned int i;
+void main(void) {
+    WDTCTL = WDTPW | WDTHOLD;           // Stop watchdog timer
 
-    P1OUT ^= 0x01;                          // Toggle P1.0 using exclusive-OR
+    P1DIR |= LedRed+LedGreen;                 // Set LED pins -> Output
+    P1DIR &= ~Przycisk;                       // Set Przycisk pin -> Input
+    P1REN |= Przycisk;                        // Enable Resistor for Przycisk pin
+    P1OUT |= Przycisk;                        // Select Pull Up for Przycisk pin
 
-    i = 10000;                              // Delay
-    do (i--);
-    while (i != 0);
-  }
+
+    int przyciskPressed = 0;
+    int ktoraSekwencja = 0;
+    if(!(P1IN & Przycisk)){
+
+        przyciskPressed++;
+        __delay_cycles(20000);
+
+    }
+    else{
+        przyciskPressed=0;
+    }
+
+    if(przyciskPressed == 1){
+        ktoraSekwencja++;
+
+    }
+
+
+
+
 }
